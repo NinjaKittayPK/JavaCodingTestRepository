377a46abdbd1c81f7efa1ad3111aadcf81c0a999
NinjaKittayPK <delbt000@gmail.com>
2021-03-13 20:48:02 -0500

    Jem's Module1Lesson1

---
+++ src/JemsTestApp.java
@@ -0,0 +1,9 @@
+public class JemsTestApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Fuck the World! Cause Teddy A Bich");
+    }
+}
+//This is how you comment a single line.
+// "main()" runs as the program is run, must be in every program.
+// the file must be named the same as what is in: public class [Name] {
--- src/ugyvkhjzergb.java
+++ src/ugyvkhjzergb.java
@@ -1,6 +1,7 @@
-public class App {
+public class ugyvkhjzergb {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Fuck the World! Cause james A Bich");
     }
 }
+//The problem that I had with your code was that "public class app {" was still there but the class needs to be the same name as the file.
