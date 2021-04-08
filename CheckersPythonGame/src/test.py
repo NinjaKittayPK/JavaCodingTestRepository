374cb3c2eaee0b41a15fc14eb0efb2a3ba6dfcf0
NinjaKittayPK <delbt000@gmail.com>
2021-04-08 15:19:21 -0400

    Teddy I Dislike Python

---
+++ CheckersPythonGame/src/test.py
@@ -0,0 +1,17 @@
+# Declaring Shite
+player1name = input('What Is The Name Of Player 1?\n')
+player1PiecesOnBoard = 12
+player1PiecesCaptured = 0
+player1Kings = 0
+
+player2name = input('What Is The Name Of Player 2?\n')
+player2PiecesOnBoard = 12
+player2PiecesCaptured = 0
+player2Kings = 0
+
+import tkinter
+
+def checkers():
+    top = tkinter.Tk()
+    top.mainloop()
+    
