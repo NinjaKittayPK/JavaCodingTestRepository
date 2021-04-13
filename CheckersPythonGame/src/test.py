204deb53e00bdf6fabec8521611627e176823dc4
NinjaKittayPK <delbt000@gmail.com>
2021-04-12 22:55:04 -0400

    terdfy

--- CheckersPythonGame/src/test.py
+++ CheckersPythonGame/src/test.py
@@ -1,5 +1,6 @@
 # Declaring Shite
 import pygame
+import tkinter
 
 player1name = input('What Is The Name Of Player 1?\n')
 player1PiecesOnBoard = 12
@@ -10,8 +11,6 @@ player2name = input('What Is The Name Of Player 2?\n')
 player2PiecesOnBoard = 12
 player2PiecesCaptured = 0
 player2Kings = 0
-
-import tkinter
 
 def checkers():
     top = tkinter.Tk()
